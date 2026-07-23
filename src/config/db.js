88f988d26dbf36b024be91f2a887526db6393ca4
viobi <vivsk0806@gmail.com>
2026-07-22 21:03:13 -0400

    contraseña de la bd

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,10 @@
 import { Pool } from 'pg';
 
-// Configuración base de la base de datos relacional
-// Los datos actuales son de ejemplo, hay que conversar que credenciales usar para la conexión 
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'proyecto_grupo3',
-    password: 'tu_clave_aqui',
+    password: 'clavada123',
     port: 5432,
 });
 
