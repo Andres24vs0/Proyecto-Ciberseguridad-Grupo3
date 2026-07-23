beb91dd29450fb2102b452d7a3c981000dfb470a
viobi <vivsk0806@gmail.com>
2026-07-23 03:15:33 -0400

    Aumentando el ataque
    para mejor visualizacion de como queda afectada la bd y lo que recibe el atacante

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -98,7 +98,7 @@ export async function updatePassword(req, res) {
         //Ruptura de autenticacion al actualizar la contraseña sin validar la identidad
         return res.status(200).json({ 
             message: "Contraseña actualizada exitosamente.",
-            usuario_modificado: result.rows[0].email
+            usuario_modificado: result.rows.email
         });
 
     } catch (error) {
